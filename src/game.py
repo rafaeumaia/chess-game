eb0a13db3b49d36b528d08cd01f6b722f497e961
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-06-12 09:59:52 -0300

    created function to show the pieces in the board, and centralize them

--- src/game.py
+++ src/game.py
@@ -1,21 +1,35 @@
 import pygame
 
 from const import *
+from board import Board
 
 
 class Game:
 
     def __init__(self):
-        pass
+        self.board = Board()
 
     def show_bg(self, surface):
         for row in range(ROWS):
             for col in range(COLS):
                 if (row + col) % 2 == 0:
-                    color = (234, 235, 200) # if the rest of the division is == 0 is a light square
+                    color = (234, 235, 200)  # if the rest of the division is == 0 is a light square
                 else:
-                    color = (119, 154, 88) # if it's not == 0 is a dark square
+                    color = (119, 154, 88)  # if it's not == 0 is a dark square
 
                 rect = (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
 
                 pygame.draw.rect(surface, color, rect)
+
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # Checking if the square has a piece
+                if self.board.squares[row][col].has_piece():
+                    # if it has store's it in the piece variable
+                    piece = self.board.squares[row][col].piece
+
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2
+                    piece.texture_rect = img.get_rect(center=img_center)
+                    surface.blit(img, piece.texture_rect)
