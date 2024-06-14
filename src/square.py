5ffb03d06d4edc168fc4686f2f61eac8ac2fe9bb
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-06-14 09:31:00 -0300

    methods to calc the valid moves of the pieces

--- src/square.py
+++ src/square.py
@@ -7,3 +7,26 @@ class Square:
 
     def has_piece(self):
         return self.piece is not None
+
+    def empty(self):
+        return not self.has_piece()
+
+    def has_rival_piece(self, color):
+        return self.has_piece() and self.piece.color is not color
+
+    def has_team_piece(self, color):
+        return self.has_piece() and self.piece.color is color
+
+    def empty_or_rival(self, color):
+        return self.empty() or self.has_rival_piece(color)
+
+    @staticmethod
+    def in_range(*args):
+        for arg in args:
+            if arg < 0 or arg > 7:
+                return False
+
+        return True
+
+
+
