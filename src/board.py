ee452b67717ca31e3ad57c58d8153a9a7ef2c981
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-06-14 09:35:30 -0300

    creathe method calc_moves and finished the knight moves method

--- src/board.py
+++ src/board.py
@@ -1,6 +1,7 @@
 from const import *
-from square import Square
 from piece import *
+from square import Square
+from move import Move
 
 
 class Board:
@@ -11,6 +12,50 @@ class Board:
         self._create()
         self._add_pieces('white')
         self._add_pieces('black')
+
+    def calc_moves(self, piece, row, col):
+        """
+            Calculates all the possible (valid) moves of a specific piece on a specific position
+        """
+
+        def knight_moves():
+            possible_moves = [
+                (row - 2, col + 1),
+                (row - 1, col + 2),
+                (row + 1, col + 2),
+                (row + 2, col + 1),
+                (row + 2, col - 1),
+                (row + 1, col - 2),
+                (row - 1, col - 2),
+                (row - 2, col - 1),
+            ]
+
+            for possible_move in possible_moves:
+                possible_move_row, possible_move_col = possible_move
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row][possible_move_col].empty_or_rival(piece.color):
+                        initial = Square(row, col)
+                        final = Square(possible_move_row, possible_move_col)
+                        move = Move(initial, final)
+                        piece.add_move(move)
+
+        if isinstance(piece, Pawn):
+            pass
+
+        elif isinstance(piece, Knight):
+            knight_moves()
+
+        elif isinstance(piece, Bishop):
+            pass
+
+        elif isinstance(piece, Rook):
+            pass
+
+        elif isinstance(piece, Queen):
+            pass
+
+        elif isinstance(piece, King):
+            pass
 
     def _create(self):
         for row in range(ROWS):
@@ -41,7 +86,3 @@ class Board:
 
         # Kings
         self.squares[row_other][4] = Square(row_other, 4, King(color))
-
-
-
-
